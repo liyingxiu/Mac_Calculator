96b216317503a770f678aa4dabf409fc3a859f0c
liyingxiu <yingxiuim@gmail.com>
2019-12-07 15:30:04 +0800

    删除js冗余代码

--- script.js
+++ script.js
@@ -1,20 +1,7 @@
-//获取所有的按键
-let btn = document.querySelector('button')
-
 //清零按键
 let btn_ac = document.querySelector('.ac');
 
-//0-9按键和dot按键，calss都是number
-// let btn_0 = document.querySelector('.zero');
-// let btn_1 = document.querySelector('.one');
-// let btn_2 = document.querySelector('.two');
-// let btn_3 = document.querySelector('.three');
-// let btn_4 = document.querySelector('.four');
-// let btn_5 = document.querySelector('.five');
-// let btn_6 = document.querySelector('.six');
-// let btn_7 = document.querySelector('.seven');
-// let btn_8 = document.querySelector('.eight');
-// let btn_9 = document.querySelector('.nine');
+//小数点按键
 let btn_dot = document.querySelector('.dot');
 
 //取反和百分数计算
@@ -28,17 +15,14 @@ let btn_minus = document.querySelector('.minus');
 let btn_plus = document.querySelector('.plus');
 let btn_equal = document.querySelector('.equal');
 
-// .calculate-area单独列出来啦
+// .calculate-area单独列出来
 let calculate_area = document.querySelector('.calculate-area');
 
 //所有的数字按键，是一个数组！！！
 let btn_num = document.getElementsByClassName('number');
 
-//所有的操作符按键,包含加减乘除和equal，不一定要用，是一个数组
+//所有的操作符按键,包含加减乘除和equal，没有用到，是一个数组
 let btn_op = document.getElementsByClassName('operator');
-
-//初始化一个arr，全局变量
-let arr = [];
 
 //设置arr1保存所有的操作,arr1中存放的每一个元素都是数字或者运算符
 let arr1 = [];
@@ -102,12 +86,8 @@ btn_mod.onclick = function () {
 
 //把加减乘除四个操作都记录到arr1中，四个运算符的操作是一样的
 btn_plus.onclick = function () {
-    //不允许连续两次输入运算操作符
     if (eval(arr1[arr1.length - 1]) != /[1-9]/) {
         arr1.push(btn_plus.value);
-    }
-    else {
-
     }
     calculate_area.innerHTML = '';
     arr = [];
@@ -127,9 +107,6 @@ btn_multiple.onclick = function () {
     if (eval(arr1[arr1.length - 1]) != /[1-9]/) {
         arr1.push(btn_multiple.value);
     }
-    else {
-
-    }
     calculate_area.innerHTML = '';
     arr = [];
     dot = 0;
@@ -138,9 +115,6 @@ btn_multiple.onclick = function () {
 btn_division.onclick = function () {
     if (eval(arr1[arr1.length - 1]) != /[1-9]/) {
         arr1.push(btn_division.value);
-    }
-    else {
-
     }
     calculate_area.innerHTML = '';
     arr = [];
